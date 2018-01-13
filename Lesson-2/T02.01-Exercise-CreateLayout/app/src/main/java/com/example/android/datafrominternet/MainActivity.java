34fb4be6732387ae5a09977605577cee9a21a781
[Muhammad Hanan] <[mhanan@aggies.ncat.edu]>
2018-01-13 18:51:10 -0500

    Lesson 2 my solutions

--- Lesson-2/T02.01-Exercise-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson-2/T02.01-Exercise-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -22,25 +22,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // COMPLETED (26) Create an EditText variable called mSearchBoxEditText
-    private EditText mSearchBoxEditText;
-
-    // COMPLETED (27) Create a TextView variable called mUrlDisplayTextView
-    private TextView mUrlDisplayTextView;
-    // COMPLETED (28) Create a TextView variable called mSearchResultsTextView
-    private TextView mSearchResultsTextView;
-
+    // TODO (26) Create an EditText variable called mSearchBoxEditText
+    EditText mSearchBoxEditText;
+    // TODO (27) Create a TextView variable called mUrlDisplayTextView
+    TextView mUrlDisplayTextView;
+    // TODO (28) Create a TextView variable called mSearchResultsTextView
+    TextView mSearchResultsTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // COMPLETED (29) Use findViewById to get a reference to mSearchBoxEditText
+        // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
         mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
-
-        // COMPLETED (30) Use findViewById to get a reference to mUrlDisplayTextView
+        // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
-        // COMPLETED (31) Use findViewById to get a reference to mSearchResultsTextView
-        mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+        // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+        mSearchResultsTextView = (TextView) findViewById(R.id.et_search_box);
     }
 }
